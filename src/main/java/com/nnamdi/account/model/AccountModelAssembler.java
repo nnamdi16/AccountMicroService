a763120f3b915c7e2dee7bf875ffb3b7ee9f470e
Nnamdi <nnwabuokei@paga.com>
2021-03-15 21:28:15 +0100

    Refactored the controller by creating an AccountModelAssembler

---
+++ src/main/java/com/nnamdi/account/model/AccountModelAssembler.java
@@ -0,0 +1,17 @@
+package com.nnamdi.account.model;
+
+import com.nnamdi.account.controller.AccountController;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountModelAssembler implements RepresentationModelAssembler<Account, EntityModel<Account>> {
+    @Override
+    public EntityModel<Account> toModel(Account account) {
+        return EntityModel.of(account,
+                WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(AccountController.class).getAccount(account.getId())).withSelfRel()
+                ,WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(AccountController.class).all()).withRel("accounts"));
+    }
+}
