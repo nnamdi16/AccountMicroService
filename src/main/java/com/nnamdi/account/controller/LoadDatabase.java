3885df36fd3debabad3d42b37d68aa0264bbcedb
Nnamdi <nnwabuokei@paga.com>
2021-03-15 22:07:07 +0100

    Intialized the db

---
+++ src/main/java/com/nnamdi/account/controller/LoadDatabase.java
@@ -0,0 +1,21 @@
+package com.nnamdi.account.controller;
+
+import com.nnamdi.account.model.Account;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoadDatabase {
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(AccountRepository repository) {
+        return args -> {
+            log.info("Preloading "  + repository.save(new Account( "Biblo Baggins", "Angel Street", "+2347038276158","valiantnnamdi@yahoo.ca","12345")));
+            log.info("Preloading "  + repository.save(new Account( "Froddo Baggins ", "Heaven Street", "+2347038200000","famola@yahoo.ca","12345")));
+        };
+    }
+}
