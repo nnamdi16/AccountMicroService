0e7bb380cfa6a03d93cb25f6f070fd19f9480fc0
Nnamdi <nnwabuokei@paga.com>
2021-03-25 16:22:29 +0100

    Added three methods in AccountRepository to search for a user by username and check if a user exist based on username and Email

--- src/main/java/com/nnamdi/account/controller/AccountRepository.java
+++ src/main/java/com/nnamdi/account/controller/AccountRepository.java
@@ -2,6 +2,13 @@ package com.nnamdi.account.controller;
 
 import com.nnamdi.account.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface AccountRepository extends JpaRepository<Account,Long> {
+    Optional<Account> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
