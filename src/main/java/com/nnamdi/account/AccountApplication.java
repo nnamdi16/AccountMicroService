512d4fa289ea83bf3d605dd2d6f81a3c59610e8e
Nnamdi <nnwabuokei@paga.com>
2021-03-25 16:54:00 +0100

    Added bCryptPasswordEncoder that encodes passwords

--- src/main/java/com/nnamdi/account/AccountApplication.java
+++ src/main/java/com/nnamdi/account/AccountApplication.java
@@ -2,12 +2,20 @@ package com.nnamdi.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @SpringBootApplication
 public class AccountApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
+	}
+
+	@Bean
+	BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return  new BCryptPasswordEncoder();
 	}
 
 }
