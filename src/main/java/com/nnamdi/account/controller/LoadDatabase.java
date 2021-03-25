f518d38df9d4ec014d937e5ee9812d03bb2c4f8d
Nnamdi <nnwabuokei@paga.com>
2021-03-25 16:24:36 +0100

    Refactored the initDatabase

--- src/main/java/com/nnamdi/account/controller/LoadDatabase.java
+++ src/main/java/com/nnamdi/account/controller/LoadDatabase.java
@@ -11,11 +11,11 @@ import org.springframework.context.annotation.Configuration;
 public class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
-    @Bean
-    CommandLineRunner initDatabase(AccountRepository repository) {
-        return args -> {
-            log.info("Preloading "  + repository.save(new Account( "Biblo Baggins", "Angel Street", "+2347038276158","valiantnnamdi@yahoo.ca","12345")));
-            log.info("Preloading "  + repository.save(new Account( "Froddo Baggins ", "Heaven Street", "+2347038200000","famola@yahoo.ca","12345")));
-        };
-    }
+//    @Bean
+//    CommandLineRunner initDatabase(AccountRepository repository) {
+//        return args -> {
+//            log.info("Preloading "  + repository.save(new Account("Badebo12345", "Biblo Baggins", "Angel Street", "+2347038276158","valiantnnamdi@yahoo.ca","12345")));
+//            log.info("Preloading "  + repository.save(new Account("Salami12345", "Froddo Baggins ", "Heaven Street", "+2347038200000","famola@yahoo.ca","12345")));
+//        };
+//    }
 }
