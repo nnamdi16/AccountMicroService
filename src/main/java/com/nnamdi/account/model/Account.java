be29c8ba23b05fea5f5259f2ad483034e314a07c
Nnamdi <nnwabuokei@paga.com>
2021-03-25 16:31:17 +0100

    Refactored Account Model adding javax validations,JPA relations and roles field

--- src/main/java/com/nnamdi/account/model/Account.java
+++ src/main/java/com/nnamdi/account/model/Account.java
@@ -2,24 +2,50 @@ package com.nnamdi.account.model;
 
 import org.springframework.hateoas.RepresentationModel;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
+@Table(name = "Account", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
 public class Account extends RepresentationModel<Account> {
-    private @Id @GeneratedValue Long id;
+
+    private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long accountId;
+
+    @NotBlank
+    @Size(max = 20)
+    private String username;
+
     private String name;
     private String address;
     private String phoneNumber;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(max = 120)
     private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "account_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
 
     public Account() {
     }
 
-    public Account(String name, String address, String phoneNumber, String email, String password) {
+    public Account(@NotBlank @Size String username, String name, String address, String phoneNumber, String email, String password) {
+        this.username = username;
         this.name = name;
         this.address = address;
         this.phoneNumber = phoneNumber;
@@ -27,12 +53,12 @@ public class Account extends RepresentationModel<Account> {
         this.password = password;
     }
 
-    public Long getId() {
-        return id;
+    public Long getAccountId() {
+        return accountId;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
     }
 
     public String getName() {
@@ -75,6 +101,14 @@ public class Account extends RepresentationModel<Account> {
         this.password = password;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
     @Override
     public  boolean equals(Object o) {
         if (this == o) {
@@ -84,26 +118,34 @@ public class Account extends RepresentationModel<Account> {
             return false;
 
         Account account = (Account) o;
-        return Objects.equals(this.id,account.id) && Objects.equals(this.name,account.name);
+        return Objects.equals(this.accountId,account.accountId) && Objects.equals(this.name,account.name);
 
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(this.id, this.name, this.address, this.password, this.email, this.phoneNumber);
+        return Objects.hash(this.accountId, this.name, this.address, this.password, this.email, this.phoneNumber);
     }
 
     @Override
     public String toString() {
         return "Account{" +
-                "id=" + id +
+                "id=" + accountId +
                 ", name='" + name + '\'' +
                 ", address='" + address + '\'' +
                 ", phoneNumber='" + phoneNumber + '\'' +
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
+                ", username='" + username + '\'' +
                 '}';
     }
 
 
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
