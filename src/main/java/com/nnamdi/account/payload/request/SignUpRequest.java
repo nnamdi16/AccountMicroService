6c0e8bf8a346bdb68bf2a7e06c72c7184e1a0393
Nnamdi <nnwabuokei@paga.com>
2021-03-25 16:35:21 +0100

    Created SignUpRequest, specifying the request payload for sign up

---
+++ src/main/java/com/nnamdi/account/payload/request/SignUpRequest.java
@@ -0,0 +1,84 @@
+package com.nnamdi.account.payload.request;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+public class SignUpRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    private String address;
+    private Set<String> role;
+    private String name;
+    private String phoneNumber;
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<String> getRole() {
+        return role;
+    }
+
+    public void setRole(Set<String> role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
