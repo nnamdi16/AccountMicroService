a09fcb356278edd24d8451c098f4f08071fe5fac
Nnamdi <nnwabuokei@paga.com>
2021-03-25 16:29:49 +0100

    Created the sql script for creating tables such as Account, Roles, UserRole

---
+++ src/main/java/com/nnamdi/account/data/userRoles.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE AccountMicroservice;
+USE AccountMicroservice;
+DROP TABLE IF EXISTS Account;
+CREATE TABLE IF NOT EXISTS Account  (
+                                        account_id INT PRIMARY KEY AUTO_INCREMENT,
+                                        name VARCHAR(500) NOT NULL,
+                                        address VARCHAR(5000) NOT NULL,
+                                        phone_number nvarchar(512) NOT NULL,
+                                        email nvarchar(100) NOT NULL,
+                                        password nvarchar(512) NOT NULL,
+                                        username nvarchar(512) NOT NULL
+
+);
+
+DROP TABLE IF EXISTS Roles;
+CREATE TABLE IF NOT EXISTS Roles (
+                                     role_id INT  PRIMARY KEY AUTO_INCREMENT,
+                                     name VARCHAR(500) NOT NULL
+
+);
+
+DROP TABLE IF EXISTS user_role;
+CREATE TABLE IF NOT EXISTS UserRole (
+                                        user_role_id INT PRIMARY KEY,
+                                        account_id INT NOT NULL ,
+                                        role_id INT(6) NOT NULL,
+                                        FOREIGN KEY (account_id) REFERENCES Account(account_id),
+                                        FOREIGN KEY (role_id) REFERENCES Roles(role_id)
+
+
+);
+
