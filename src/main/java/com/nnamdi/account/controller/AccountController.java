484465f0be4fc418464ccf90fc3e17c901698508
Nnamdi <nnwabuokei@paga.com>
2021-03-25 16:21:13 +0100

    Refactored the AccountController class taken out authentication based methods such as methods that deals with sign up and login

--- src/main/java/com/nnamdi/account/controller/AccountController.java
+++ src/main/java/com/nnamdi/account/controller/AccountController.java
@@ -3,48 +3,60 @@ package com.nnamdi.account.controller;
 import com.nnamdi.account.exceptions.AccountNotFoundException;
 import com.nnamdi.account.model.Account;
 import com.nnamdi.account.model.AccountModelAssembler;
+import com.nnamdi.account.security.jwt.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
+@RequestMapping("/api/accounts")
 public class AccountController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
     private final AccountRepository accountRepository;
+    private final RoleRepository roleRepository;
     private final AccountModelAssembler accountModelAssembler;
 
-    public AccountController(AccountRepository accountRepository,AccountModelAssembler accountModelAssembler) {
+    public AccountController(AccountRepository accountRepository, RoleRepository roleRepository, AccountModelAssembler accountModelAssembler, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.accountRepository = accountRepository;
+        this.roleRepository = roleRepository;
         this.accountModelAssembler = accountModelAssembler;
     }
 
+
+
+
     //Aggregate root
     //tag::get-aggregate-root[]
-    @GetMapping("/accounts")
+    @GetMapping("/all")
+    @PreAuthorize("hasRole('ADMIN')")
     public CollectionModel<EntityModel<Account>> all() {
         List<EntityModel<Account>> accounts = accountRepository.findAll().stream().map(accountModelAssembler::toModel).collect(Collectors.toList());
         return CollectionModel.of(accounts, WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(AccountController.class).all()).withSelfRel());
     }
-//    List<Account>all() {
-//        return accountRepository.findAll();
-//    }
-    //end:: get-aggregate-root[]
 
-    @PostMapping("/accounts")
-    public ResponseEntity<?> registerAccount(@RequestBody Account registerAccount) {
-        EntityModel<Account> entityModel = accountModelAssembler.toModel(accountRepository.save(registerAccount));
-        return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
-    }
 
 
     //Single item
 
-    @GetMapping("/accounts/{id}")
+    @GetMapping("/account/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public EntityModel<Account> getAccount(@PathVariable Long id){
         Account account = accountRepository.findById(id).orElseThrow(() -> new AccountNotFoundException(id));
         return accountModelAssembler.toModel(account);
@@ -52,7 +64,8 @@ public class AccountController {
 
 
 
-    @PutMapping("/accounts/{id}")
+    @PutMapping("/account/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> updateAccount(@RequestBody Account account, @PathVariable Long id) {
         Account updateAccount = accountRepository.findById(id)
                 .map(userAccount ->{
@@ -63,7 +76,7 @@ public class AccountController {
                     userAccount.setPhoneNumber(account.getPhoneNumber());
                     return accountRepository.save(userAccount);
                 })    .orElseGet(() -> {
-                    account.setId(id);
+                    account.setAccountId(id);
                     return accountRepository.save(account);
                 });
         EntityModel<Account> entityModel = accountModelAssembler.toModel(updateAccount);
@@ -71,7 +84,8 @@ public class AccountController {
     }
 
 
-    @DeleteMapping("/accounts/{id}")
+    @DeleteMapping("/account/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     ResponseEntity<?> deleteAccount(@PathVariable Long id) {
         accountRepository.deleteById(id);
         return ResponseEntity.noContent().build();
