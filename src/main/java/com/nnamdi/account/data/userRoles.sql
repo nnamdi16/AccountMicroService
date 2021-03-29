7603f5c34a76329a2856dd0d89a0f6a2ffb4f7c9
Nnamdi <nnwabuokei@paga.com>
2021-03-29 23:07:58 +0100

    Refactored userRoles.sql modifying instances that allows for dropping of the database

--- src/main/java/com/nnamdi/account/data/userRoles.sql
+++ src/main/java/com/nnamdi/account/data/userRoles.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE AccountMicroservice;
-USE AccountMicroservice;
-DROP TABLE IF EXISTS Account;
+CREATE DATABASE IF NOT EXISTS  AccountMicroservice;
+# USE AccountMicroservice;
+# DROP TABLE IF EXISTS Account;
 CREATE TABLE IF NOT EXISTS Account  (
                                         account_id INT PRIMARY KEY AUTO_INCREMENT,
                                         name VARCHAR(500) NOT NULL,
@@ -12,14 +12,14 @@ CREATE TABLE IF NOT EXISTS Account  (
 
 );
 
-DROP TABLE IF EXISTS Roles;
+# DROP TABLE IF EXISTS Roles;
 CREATE TABLE IF NOT EXISTS Roles (
                                      role_id INT  PRIMARY KEY AUTO_INCREMENT,
                                      name VARCHAR(500) NOT NULL
 
 );
 
-DROP TABLE IF EXISTS user_role;
+# DROP TABLE IF EXISTS user_role;
 CREATE TABLE IF NOT EXISTS UserRole (
                                         user_role_id INT PRIMARY KEY,
                                         account_id INT NOT NULL ,
