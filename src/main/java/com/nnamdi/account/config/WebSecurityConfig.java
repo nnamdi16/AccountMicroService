e8f7e0709b7148e49e41aa8ce7e6436c7dbf38c7
Nnamdi <nnwabuokei@paga.com>
2021-03-30 20:13:13 +0100

    Refactored WebSecurityConfig

--- src/main/java/com/nnamdi/account/config/WebSecurityConfig.java
+++ src/main/java/com/nnamdi/account/config/WebSecurityConfig.java
@@ -2,6 +2,8 @@ package com.nnamdi.account.config;
 
 import com.nnamdi.account.security.jwt.AuthEntryPointJwt;
 import com.nnamdi.account.security.jwt.AuthTokenFilter;
+import com.nnamdi.account.security.jwt.JwtTokenFilterConfigurer;
+import com.nnamdi.account.security.jwt.JwtTokenProvider;
 import com.nnamdi.account.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,16 +23,19 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-     @Autowired
-     UserDetailsServiceImpl userDetailsService;
-
     @Autowired
-    private AuthEntryPointJwt unauthorizedHandler;
+    private JwtTokenProvider jwtTokenProvider;
 
-    @Bean
-    public AuthTokenFilter authenticationJwtTokenFilter() {
-        return new AuthTokenFilter();
-    }
+//    @Autowired
+//     UserDetailsServiceImpl userDetailsService;
+//
+//    @Autowired
+//    private AuthEntryPointJwt unauthorizedHandler;
+
+//    @Bean
+//    public AuthTokenFilter authenticationJwtTokenFilter() {
+//        return new AuthTokenFilter();
+//    }
 
     @Bean
     @Override
@@ -39,18 +45,51 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return  new BCryptPasswordEncoder();
+        return  new BCryptPasswordEncoder(12);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/accounts/**").permitAll()
+        // Disable CSRF (cross site request forgery)
+        http.csrf().disable();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        //Entry points
+        http.authorizeRequests()//
+                .antMatchers("/api/auth/**").permitAll()//
+                .antMatchers("/api/accounts/roles").permitAll()//
+                .antMatchers("/api/accounts/ban/**").permitAll()//
+                // Disallow everything else..
                 .anyRequest().authenticated();
 
-        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        // If a user try to access a resource without having enough permissions
+        http.exceptionHandling().accessDeniedPage("/api/auth/login");
+
+//                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+//                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
+//                .antMatchers("/api/accounts/**").permitAll()
+//                .anyRequest().authenticated();
+
+//        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // Allow swagger to be accessed without authentication
+        web.ignoring().antMatchers("/v2/api-docs")//
+                .antMatchers("/swagger-resources/**")//
+                .antMatchers("/swagger-ui.html")//
+                .antMatchers("/configuration/**")//
+                .antMatchers("/webjars/**")//
+                .antMatchers("/public")
+
+                // Un-secure H2 Database (for testing purposes, H2 console shouldn't be unprotected in production)
+                .and()
+                .ignoring()
+                .antMatchers("/h2-console/**/**");;
+    }
+
 }
