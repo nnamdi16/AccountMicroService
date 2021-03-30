d5275ea23cacfe117cd828206eb0bbf286bc7b46
Nnamdi <nnwabuokei@paga.com>
2021-03-30 22:48:05 +0100

    Refactored AuthenticationController moving the logic to UserService.java

--- src/main/java/com/nnamdi/account/controller/AuthController.java
+++ src/main/java/com/nnamdi/account/controller/AuthController.java
@@ -8,53 +8,40 @@ import com.nnamdi.account.payload.response.MessageResponse;
 import com.nnamdi.account.repository.AccountRepository;
 import com.nnamdi.account.repository.RoleRepository;
 import com.nnamdi.account.repository.UserRoleRepository;
-import com.nnamdi.account.security.jwt.JwtUtils;
-import com.nnamdi.account.service.UserDetailsImpl;
+import com.nnamdi.account.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    @Autowired
-    AuthenticationManager authenticationManager;
 
     @Autowired
-    JwtUtils jwtUtils;
+    UserService userService;
 
     private final AccountRepository accountRepository;
-    private final UserRoleRepository userRoleRepository;
-    private final RoleRepository roleRepository;
+
     private final AccountModelAssembler accountModelAssembler;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
 
     public AuthController(AccountRepository accountRepository, UserRoleRepository userRoleRepository, RoleRepository roleRepository, AccountModelAssembler accountModelAssembler, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.accountRepository = accountRepository;
-        this.userRoleRepository = userRoleRepository;
-        this.roleRepository = roleRepository;
         this.accountModelAssembler = accountModelAssembler;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+
     }
 
 
 
-    @PostMapping("/accounts")
+    @PostMapping("/signup")
     @Transactional(rollbackFor = Exception.class)
     public ResponseEntity<?> registerAccount(@Valid @RequestBody SignUpRequest signUpRequest) {
         if (accountRepository.existsByUsername(signUpRequest.getUsername())) {
@@ -64,50 +51,17 @@ public class AuthController {
         if (accountRepository.existsByEmail(signUpRequest.getEmail())) {
             return ResponseEntity.badRequest().body(new MessageResponse("Error:email is already in use"));
         }
-        Account registerAccount = new Account(signUpRequest.getUsername(),signUpRequest.getName(), signUpRequest.getAddress(), signUpRequest.getPhoneNumber(), signUpRequest.getEmail(),bCryptPasswordEncoder.encode(signUpRequest.getPassword()) );
-        Set<String> strRoles = signUpRequest.getRole();
-        Set<Roles> roles = new HashSet<>();
 
-        if (strRoles == null) {
-            Roles userUserRoles = roleRepository.findByName(RolesEnum.ROLE_USER)
-                    .orElseThrow(()-> new RuntimeException("Error: Role does not exist"));
-            roles.add(userUserRoles);
-        } else {
-            strRoles.forEach(role -> {
-                switch (role) {
-                    case "admin":
-                        Roles adminUserRoles = roleRepository.findRolesByName(RolesEnum.ROLE_ADMIN)
-                                .orElseThrow(() -> new RuntimeException("Error: Role is not found. "));
-                        roles.add(adminUserRoles);
-                        break;
-
-                    default:
-                        Roles userUserRoles = roleRepository.findRolesByName(RolesEnum.ROLE_USER).orElseThrow(() -> new RuntimeException("Error:Role is not found"));
-                        roles.add(userUserRoles);
-                }
-            });
-        }
-        registerAccount.setRoles(roles);
-        accountRepository.save(registerAccount);
-        EntityModel<Account> entityModel = accountModelAssembler.toModel(accountRepository.save(registerAccount));
+        Account userAccount = userService.signup(signUpRequest);
+        accountRepository.save(userAccount);
+        EntityModel<Account> entityModel = accountModelAssembler.toModel(accountRepository.save(userAccount));
         return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),loginRequest.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = jwtUtils.generateJwtToken(authentication);
-
-        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        List<String> roles = userDetails.getAuthorities().stream()
-                .map(item->item.getAuthority())
-                .collect(Collectors.toList());
-        System.out.println(roles);
-        return ResponseEntity.ok(new JwtResponse(jwt,
-                userDetails.getId(),
-                userDetails.getUsername(),
-                userDetails.getEmail(),roles));
+        JwtResponse jwtResponse = userService.signin(loginRequest.getUsername(),loginRequest.getPassword());
+        return ResponseEntity.ok(jwtResponse);
 
     }
 
--- src/main/java/com/nnamdi/account/service/UserService.java
+++ src/main/java/com/nnamdi/account/service/UserService.java
@@ -1,66 +1,92 @@
-//package com.nnamdi.account.service;
-//
-//import com.nnamdi.account.exceptions.CustomException;
-//import com.nnamdi.account.repository.AccountRepository;
-//import com.nnamdi.account.security.jwt.JwtTokenProvider;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.AuthenticationException;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class UserService {
-//    @Autowired
-//    private AccountRepository accountRepository;
-//
-//    @Autowired
-//    private PasswordEncoder passwordEncoder;
-//
-//    @Autowired
-//    private JwtTokenProvider jwtTokenProvider;
-//
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
-//
-//    public String signin(String username, String password) {
-//        try {
-//            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-//            return jwtTokenProvider.createToken(username, accountRepository.findByUsername(username));
-//        } catch (AuthenticationException e) {
-//            throw new CustomException("Invalid username/password supplied", HttpStatus.UNPROCESSABLE_ENTITY);
-//        }
-//    }
-//
-////    public String signup( user) {
-////        if (!userRepository.existsByUsername(user.getUsername())) {
-////            user.setPassword(passwordEncoder.encode(user.getPassword()));
-////            userRepository.save(user);
-////            return jwtTokenProvider.createToken(user.getUsername(), user.getRoles());
-////        } else {
-////            throw new CustomException("Username is already in use", HttpStatus.UNPROCESSABLE_ENTITY);
-////        }
-////    }
-//
-////    public void delete(String username) {
-////        userRepository.deleteByUsername(username);
-////    }
-////
-////    public User search(String username) {
-////        User user = userRepository.findByUsername(username);
-////        if (user == null) {
-////            throw new CustomException("The user doesn't exist", HttpStatus.NOT_FOUND);
-////        }
-////        return user;
-////    }
-////
-////    public User whoami(HttpServletRequest req) {
-////        return userRepository.findByUsername(jwtTokenProvider.getUsername(jwtTokenProvider.resolveToken(req)));
-////    }
-////
-////    public String refresh(String username) {
-////        return jwtTokenProvider.createToken(username, userRepository.findByUsername(username).getRoles());
-////    }
-//}
+package com.nnamdi.account.service;
+
+import com.nnamdi.account.exceptions.CustomException;
+import com.nnamdi.account.model.Account;
+import com.nnamdi.account.model.Roles;
+import com.nnamdi.account.model.RolesEnum;
+import com.nnamdi.account.payload.request.SignUpRequest;
+import com.nnamdi.account.payload.response.JwtResponse;
+import com.nnamdi.account.repository.RoleRepository;
+import com.nnamdi.account.security.jwt.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Autowired
+    private  BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    public JwtResponse signin(String username, String password) {
+        try {
+
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            String jwt = jwtUtils.generateJwtToken(authentication);
+            UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+            List<String> roles = userDetails.getAuthorities().stream()
+                    .map(item->item.getAuthority())
+                    .collect(Collectors.toList());
+            JwtResponse jwtResponse = new JwtResponse(jwt,
+                    userDetails.getId(),
+                    userDetails.getUsername(),
+                    userDetails.getEmail(),roles);
+            return jwtResponse;
+
+        } catch (AuthenticationException e) {
+            throw new CustomException("Invalid username/password supplied", HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+    }
+
+    public Account signup(SignUpRequest signUpRequest) {
+        Account registerAccount = new Account(signUpRequest.getUsername(),signUpRequest.getName(), signUpRequest.getAddress(), signUpRequest.getPhoneNumber(), signUpRequest.getEmail(),bCryptPasswordEncoder.encode(signUpRequest.getPassword()) );
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Roles> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Roles userUserRoles = roleRepository.findByName(RolesEnum.ROLE_USER)
+                    .orElseThrow(()-> new RuntimeException("Error: Role does not exist"));
+            roles.add(userUserRoles);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Roles adminUserRoles = roleRepository.findRolesByName(RolesEnum.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found. "));
+                        roles.add(adminUserRoles);
+                        break;
+
+                    default:
+                        Roles userUserRoles = roleRepository.findRolesByName(RolesEnum.ROLE_USER).orElseThrow(() -> new RuntimeException("Error:Role is not found"));
+                        roles.add(userUserRoles);
+                }
+            });
+        }
+        registerAccount.setRoles(roles);
+        return registerAccount;
+
+    }
+
+
+}
