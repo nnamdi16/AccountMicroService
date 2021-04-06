a3ba5414f9ed02bbd267c32e243723e9f3f3ac32
Nnamdi <nnwabuokei@paga.com>
2021-04-06 18:33:53 +0100

    Changed the RabbitMqSender to MessageChannel, used it to communicate with Notification Service

--- src/main/java/com/nnamdi/account/controller/AuthController.java
+++ src/main/java/com/nnamdi/account/controller/AuthController.java
@@ -8,12 +8,20 @@ import com.nnamdi.account.payload.response.MessageResponse;
 import com.nnamdi.account.repository.AccountRepository;
 import com.nnamdi.account.repository.RoleRepository;
 import com.nnamdi.account.repository.UserRoleRepository;
-import com.nnamdi.account.service.RabbitMqSender;
+import com.nnamdi.account.service.MessageService;
+//import com.nnamdi.account.service.RabbitMqSender;
 import com.nnamdi.account.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -24,25 +32,33 @@ import javax.validation.Valid;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
+@Api(tags = "accounts", value = "Account Microservice")
 public class AuthController {
 
     @Autowired
     UserService userService;
-    private RabbitMqSender rabbitMqSender;
+//    private RabbitMqSender rabbitMqSender;
+
+    private MessageChannel messageChannel;
 
     private final AccountRepository accountRepository;
 
     private final AccountModelAssembler accountModelAssembler;
 
 
-    public AuthController(AccountRepository accountRepository, UserRoleRepository userRoleRepository, RoleRepository roleRepository, AccountModelAssembler accountModelAssembler, BCryptPasswordEncoder bCryptPasswordEncoder, RabbitMqSender rabbitMqSender) {
+    public AuthController(AccountRepository accountRepository, AccountModelAssembler accountModelAssembler, MessageService messageService) {
         this.accountRepository = accountRepository;
         this.accountModelAssembler = accountModelAssembler;
-        this.rabbitMqSender = rabbitMqSender;
+//        this.rabbitMqSender = rabbitMqSender;
+        messageChannel = messageService.notification();
     }
 
 
-
+    @ApiOperation(value = "Create an account")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong "),
+            @ApiResponse(code = 422, message = "Username or email already exist")
+    })
     @PostMapping("/signup")
     @Transactional(rollbackFor = Exception.class)
     public ResponseEntity<?> registerAccount(@Valid @RequestBody SignUpRequest signUpRequest) {
@@ -56,11 +72,17 @@ public class AuthController {
 
         Account userAccount = userService.signup(signUpRequest);
         accountRepository.save(userAccount);
-        rabbitMqSender.send(userAccount.getEmail());
+        Message<String> msg = MessageBuilder.withPayload(userAccount.getEmail()).build();
+        this.messageChannel.send(msg);
+//        rabbitMqSender.send(userAccount.getEmail());
         EntityModel<Account> entityModel = accountModelAssembler.toModel(accountRepository.save(userAccount));
         return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
     }
 
+    @ApiOperation(value ="Authenticates user and returns its JWT token." )
+    @ApiResponses(value = {
+            @ApiResponse(code = 400,message = "Something went wrong")
+    })
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
         JwtResponse jwtResponse = userService.signin(loginRequest.getUsername(),loginRequest.getPassword());
