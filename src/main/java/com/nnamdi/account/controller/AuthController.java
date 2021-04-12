83423d120580478f777423d52fd031722f845260
Nonso Nwabuokei <nnwabuokei@gmail.com>
2021-04-12 09:12:26 +0100

    Refactored AuthController created a Message of type Array list of String using MessageBuilder using .send() method on the MessageChannel to publish the message.

--- src/main/java/com/nnamdi/account/controller/AuthController.java
+++ src/main/java/com/nnamdi/account/controller/AuthController.java
@@ -6,15 +6,16 @@ import com.nnamdi.account.payload.request.SignUpRequest;
 import com.nnamdi.account.payload.response.JwtResponse;
 import com.nnamdi.account.payload.response.MessageResponse;
 import com.nnamdi.account.repository.AccountRepository;
-import com.nnamdi.account.repository.RoleRepository;
-import com.nnamdi.account.repository.UserRoleRepository;
-import com.nnamdi.account.service.MessageService;
+import com.nnamdi.account.service.AccountRequestProvider;
+import com.nnamdi.account.service.MessageSenderService;
 //import com.nnamdi.account.service.RabbitMqSender;
 import com.nnamdi.account.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
@@ -22,11 +23,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -38,19 +40,23 @@ public class AuthController {
     @Autowired
     UserService userService;
 //    private RabbitMqSender rabbitMqSender;
+    Logger logger = LoggerFactory.getLogger(AuthController.class);
 
-    private MessageChannel messageChannel;
+//    private MessageChannel messageChannel;
 
     private final AccountRepository accountRepository;
 
     private final AccountModelAssembler accountModelAssembler;
 
+    private final AccountRequestProvider accountRequestProvider;
 
-    public AuthController(AccountRepository accountRepository, AccountModelAssembler accountModelAssembler, MessageService messageService) {
+
+    public AuthController(AccountRepository accountRepository, AccountModelAssembler accountModelAssembler, AccountRequestProvider accountRequestProvider) {
         this.accountRepository = accountRepository;
         this.accountModelAssembler = accountModelAssembler;
 //        this.rabbitMqSender = rabbitMqSender;
-        messageChannel = messageService.notification();
+//        messageChannel = messageSenderService.notification();
+        this.accountRequestProvider = accountRequestProvider;
     }
 
 
@@ -72,8 +78,14 @@ public class AuthController {
 
         Account userAccount = userService.signup(signUpRequest);
         accountRepository.save(userAccount);
-        Message<String> msg = MessageBuilder.withPayload(userAccount.getEmail()).build();
-        this.messageChannel.send(msg);
+        List<String> accountNotification = new ArrayList<>();
+        String accountId = String.valueOf(userAccount.getAccountId());
+        accountNotification.add(accountId);
+        accountNotification.add(userAccount.getEmail());
+        accountRequestProvider.requestApproval(accountNotification);
+//        Message<List<String>> msg = MessageBuilder.withPayload(accountNotification).build();
+//        this.messageChannel.send(msg);
+//        logger.info(String.valueOf(msg));
 //        rabbitMqSender.send(userAccount.getEmail());
         EntityModel<Account> entityModel = accountModelAssembler.toModel(accountRepository.save(userAccount));
         return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
